5157431b443fc046c4eea00a100a82b57e19b6d8
priyankagat-webonise <gatpriyanka@gmail.com>
2016-03-31 12:36:00 +0530

    add modified databaseBackupFile

---
+++ databaseBackup.sh
@@ -0,0 +1,6 @@
+ today=`date +%Y-%m-%d.%H:%M:%S`
+ backup_path="/home/webonise/db_backup/"
+ umask 777
+ mysqldump --user="root" --password="system" --host="127.0.0.1" "DBMS"> $backup_path/"DBMS+$today".sql
+
+
