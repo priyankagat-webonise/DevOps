34da7bb9d5c145a4a07a61acd7017ada61fccecd
priyankagat-webonise <gatpriyanka@gmail.com>
2016-03-31 12:34:05 +0530

    added capistrano modified

---
+++ capistrano/config/deploy.rb
@@ -0,0 +1,8 @@
+
+set :application, 'demo'
+set :scm, :git
+set :repo_url, 'https://github.com/priyankagat-webonise/capistrano.git'
+set :deploy_to, "/home/webonise/deploy/priyanka"
+role :web, "10.0.0.143"
+
+
